94773fb6136f9a312e3d603bf45eceb78ba1806e
jahnavibarma03 <144089718+jahnavibarma03@users.noreply.github.com>
2024-01-03 11:29:25 +0530

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,66 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs",
+      name: "Casual Shirt",
+      price: 450,
+      desc: "Men Slim Fit Solid Slim Collar Casual Shirt",
+      img: "images/img1.jpg",
+    },
+    {
+      id: "ioytrhndcv",
+      name: "Office Shirt",
+      price: 1000,
+      desc: "Men Regular Fit Solid Spread Collar Formal Shirt",
+      img: "images/img-2.jpg",
+    },
+    {
+      id: "wuefbncxbsn",
+      name: "T Shirt",
+      price: 250,
+      desc: "Men Graphic Print, Solid Round Neck Pure Cotton Orange T-Shirt",
+      img: "images/img-3.jpg",
+    },
+    {
+      id: "thyfhcbcv",
+      name: "Mens Suit",
+      price: 3000,
+      desc: "Men SLIM FIT Solid Suit",
+      img: "images/img-4.jpg",
+    },
+    
+      {
+        id: "jeyfhcuycvdr",
+        name: "High Star Jeans",
+        price: 599,
+        desc: "Straight Fit High-Rise Stretchable Jeans",
+        img: "images/img-5.jpg",
+      },
+      {
+        id: "hodfhcuycvdr",
+        name: "Hoodie",
+        price: 699,
+        desc: "Women Grey Hooded Sweatshirt",
+        img: "images/img-6.jpg",
+      },
+      {
+        id: "kuodfhcuycvdr",
+        name: "Libas Kurta",
+        price: 584,
+        desc: "Women Blue & Pink Floral Motifs Printed Kurta",
+        img: "images/img-7.jpg",
+      },
+      {
+        id: "kurtfhcuycvdr",
+        name: "Sangria Kurta",
+        price: 584,
+        desc: "Ethnic Motifs Embroidered A-Line Kurta With Dupatta",
+        img: "images/img-8.jpg",
+      }, 
+      {
+        id: "kurtfhcuycvdr",
+        name: "Athena Top",
+        price: 389,
+        desc: "Chic Fuchsia Pink Power Shoulders Top",
+        img: "images/img-9.jpg",
+      }, 
+  ];
